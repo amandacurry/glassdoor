f5490af0ce5d65ca6d80ce144dad31dabfc1a876
amandacurry <amanda.cercas@gmail.com>
2021-12-14 11:37:35 +0100

    Scraper updates

--- scrape_glassdoor.sh
+++ scrape_glassdoor.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd ./glassdoor-review-scraper/
+cd ./glassdoor_scraper/
 python main.py --headless --url "https://www.glassdoor.com/Overview/Working-at-Amazon-EI_IE6036.11,17.htm" --limit 200000 -f amazon_reviews.csv
 python main.py --headless --url "https://www.glassdoor.com/facebook" --limit 200000 -f facebook_reviews.csv
 python main.py --headless --url "https://www.glassdoor.com/Overview/Working-at-Google-EI_IE9079.11,17.htm" --limit 200000 -f google_reviews.csv
